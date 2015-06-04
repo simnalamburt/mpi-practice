606addc437f48782511d7e79262314127e306e1b
Hyeon Kim <simnalamburt@gmail.com>
2015-06-04 17:00:51 +0900

    misc

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  int my_rank, size;
+  int rank, size;
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  printf("Hello, I am %d of %d\n", my_rank, size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  printf("Hello, I am %d of %d\n", rank, size);
   MPI_Finalize();
   return 0;
 }
